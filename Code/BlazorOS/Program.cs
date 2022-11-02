5b1d456d4ad8376bed7a9e455907390ffb639b33
Rosabm <rosabrialesm@gmail.com>
2022-11-02 18:55:38 +0100

    Create socks blazor page
    I modified the page to follow our design, I also added dependencies for SMART (we can get components for our blazor that look nice from there, i used it for the dropdown list)

--- Code/BlazorOS/Program.cs
+++ Code/BlazorOS/Program.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorOS;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using Smart.Blazor;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<ISockService, SockHttpClient>();
-
+builder.Services.AddSmart();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7999") });
 
 
