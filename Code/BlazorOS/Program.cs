d5ecaad0eb84ea20fdf96a1d6635abea1c5e676d
Rosabm <rosabrialesm@gmail.com>
2022-11-02 19:13:49 +0100

    create socks page

--- Code/BlazorOS/Program.cs
+++ Code/BlazorOS/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorOS;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
+using Smart.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<ISockService, SockHttpClient>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7999") });
+builder.Services.AddSmart();  
 
 
 await builder.Build().RunAsync();
