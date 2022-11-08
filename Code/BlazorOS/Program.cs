861e10cc25d48c09bac653229c60b6821093770e
Alex Dulghier <92308490+kimintor@users.noreply.github.com>
2022-11-08 15:29:53 +0100

    try1

--- Code/BlazorOS/Program.cs
+++ Code/BlazorOS/Program.cs
@@ -11,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<ISockService, SockHttpClient>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7999") });
-builder.Services.AddSmart();  
+builder.Services.AddSmart();
 
 await builder.Build().RunAsync();
