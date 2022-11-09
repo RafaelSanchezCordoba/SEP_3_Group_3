65b2eea4668d010103d8cdafc796dd0bad74fbc8
Alex Dulghier <92308490+kimintor@users.noreply.github.com>
2022-11-09 13:03:58 +0100

    ...

---
+++ Code/T1_SEP3_/Domain/Models/ProductCard.cs
@@ -0,0 +1,28 @@
+namespace Shared.Models;
+
+public class ProductCard
+{
+    //We need to add a title to the product card.
+    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string Material { get; set; }
+    public string Brand { get; set; }
+    public string Image { get; set; }
+    public string Type { get; set; }
+    public int Id { get; set; }
+
+
+
+    protected ProductCard(string title, string description, double price, string material, string brand, string image, string type)
+    {
+        Title = title;
+        Description = description;
+        Price = price;
+        Material = material;
+        Brand = brand;
+        Image = image;
+        Type = type;
+    }
+}
