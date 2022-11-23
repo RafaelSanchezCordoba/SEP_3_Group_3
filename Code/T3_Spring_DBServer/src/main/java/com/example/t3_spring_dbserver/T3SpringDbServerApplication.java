767f5d7c3672bbfbe6779bf02ca734bc6c8b630c
Alex Dulghier <92308490+kimintor@users.noreply.github.com>
2022-11-23 10:56:03 +0100

    base spring project

---
+++ Code/T3_Spring_DBServer/src/main/java/com/example/t3_spring_dbserver/T3SpringDbServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.t3_spring_dbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3SpringDbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3SpringDbServerApplication.class, args);
+	}
+
+}
